144323c24791ac32a0bbc749668b6486f9daa4c5
Conqu3red <Conqu3red@users.noreply.github.com>
2021-06-13 15:53:40 +0100

    fix save location

--- convert.py
+++ convert.py
@@ -3,6 +3,6 @@ import os
 
 location = input("enter json file location: ")
 t = jsonformat.load_json(location)
-save_name = os.path.splitext(os.path.basename(location))[0]
+save_name = os.path.splitext(os.path.basename(location))[0] + ".trk"
 trkformat.save_trk(t, save_name)
-print(f"Saved to {save_name}.trk")
+print(f"Saved to {save_name}")
